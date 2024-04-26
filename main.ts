31a8c476cc7deae9508bbf8c8bb1246498bfefb2
Umama-shaikh <160320031+Umama-shaikh@users.noreply.github.com>
2024-04-26 20:53:41 +0500

    created atm machine

---
+++ main.ts
@@ -0,0 +1,61 @@
+#! /usr/bin/env node 
+
+import inquirer from "inquirer";
+
+let myBalance = 10000; // dollar
+let pincode = 4567;
+
+let pinAnswer = await inquirer.prompt([
+    {
+        name : "pin",
+        message : "enter your pin",
+        type : "number"
+
+    },
+]);
+
+if (pinAnswer.pin === pincode) {
+    console.log("correct pin code!!!");
+    let operationAns = await inquirer.prompt(
+        [
+            {
+                name : "operation",
+                message : "please select option",
+                type : "list",
+                choices : ["withdraw","check balance"]
+            }
+
+        ]
+
+    );
+    console.log(operationAns);
+
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt(
+            [
+                {
+                    name : "amount",
+                    message : "enter your amount",
+                    type : "number"
+                }
+            ]
+        );
+
+        
+        
+        if (myBalance >= amountAns.amount) {
+            console.log(myBalance -= amountAns.amount);
+            console.log(`your remaining balance is: ${myBalance}`);
+        }else {
+            console.log("insufficient balance");
+        }
+
+    }else if (operationAns.operation === "check balance") {
+        console.log(`your balance is ${myBalance}`);
+    }
+    
+} 
+
+else {
+    console.log("incorrect pin number");
+}
